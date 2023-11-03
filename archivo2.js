5b417d3e7e9229c9a1cb6bc0bbb522f04ac81531
Leandro <lea_romero@hotmail.com>
2023-11-03 19:56:59 -0300

    finalizado

---
+++ archivo2.js
@@ -0,0 +1,49 @@
+const nombre = document.getElementById("nombre")
+const apellido = document.getElementById("apellido")
+const ciudad = document.getElementById("ciudad")
+const email = document.getElementById("email")
+const telefono = document.getElementById("telefono")
+const destino = document.getElementById("destino")
+const comentario = document.getElementById("textarea")
+const form = document.getElementById("form")
+const parrafo = document.getElementById("warnings")
+const inquietud = document.getElementById("inquietud")
+
+form.addEventListener("submit", e=>{
+    e.preventDefault()
+    let warnings = ""
+    let entrar = false
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+if(nombre.value.length <3){
+   warnings += `El nombre no es válido <br>`
+   entrar = true
+}
+if(apellido.value.length <2){
+   warnings += `El apellido no es válido <br>`
+    entrar = true
+ }
+ if(ciudad.value.length <4){
+   warnings += `La ciudad no es válida <br>`
+    entrar = true
+ }
+ if(telefono.value.length <10){
+   warnings += `El teléfono no es válido <br>`
+    entrar = true
+ }
+ if(destino.value.length <5){
+    warnings += `Nuestra agencia no cuenta con ese destino <br>`
+    entrar = true
+ }
+ if(textarea.value.length <10){
+    warnings += `Escriba por lo menos 10 palabras sobre su experiencia <br>`
+ }
+console.log(regexEmail.test(email.value))
+if(!regexEmail.test(email.value)){
+   warnings += `El email no es válido <br>`
+   entrar = true
+}
+if(entrar){
+   parrafo.innerHTML = warnings
+}else{parrafo.innerHTML = "Enviado"
+}
+})
--- script.js
+++ script.js
@@ -1,42 +1,11 @@
-let slideIndex = 0;
-showSlides();
-let slideIndex1 = 0;
-showSlides1();
-let slideIndex2 = 0;
-showSlides2();
+const menu = document.querySelector("#menu");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
 
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
+abrir.addEventListener("click", () => {
+  menu.classList.add("visible");
+});
 
-function showSlides1() {
-    let i;
-    let slides = document.getElementsByClassName("mySlides1");
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    slideIndex1++;
-    if (slideIndex1 > slides.length) {slideIndex1 = 1}
-    slides[slideIndex1-1].style.display = "block";
-    setTimeout(showSlides1, 2000); // Change image every 2 seconds
-  }
-
-  function showSlides2() {
-    let i;
-    let slides = document.getElementsByClassName("mySlides2");
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    slideIndex2++;
-    if (slideIndex2 > slides.length) {slideIndex2 = 1}
-    slides[slideIndex2-1].style.display = "block";
-    setTimeout(showSlides2, 2000); // Change image every 2 seconds
-  }
+cerrar.addEventListener("click", () => {
+  menu.classList.remove("visible");
+});
